99e04d13d553253534401d134e48a72889dd57f8
melihes <melihesmeyar@gmail.com>
2026-07-25 16:11:40 +0300

    Refactor audio playback and connection handling

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const {
     AudioPlayerStatus,
     VoiceConnectionStatus,
     StreamType,
-    entersState
+    NoSubscriberBehavior
 } = require('@discordjs/voice');
 const http = require('http');
 const path = require('path');
 const ffmpegPath = require('ffmpeg-static');
 const fs = require('fs');
-const sodium = require('libsodium-wrappers');
 
-// Web Sunucusu (Uptime için)
 http.createServer((req, res) => {
     res.write("Bot 7/24 Aktif!");
     res.end();
@@ -30,10 +28,6 @@ const client = new Client({
 client.on('clientReady', async () => {
     console.log(`>>> BOT AKTİF: ${client.user.tag}`);
 
-    // Sodium şifreleme kütüphanesinin hazır olmasını bekliyoruz
-    await sodium.ready;
-    console.log(">>> SHİFRELEME (SODIUM) HAZIR!");
-
     const channelId = process.env.KANAL_ID;
     
     try {
@@ -45,49 +39,50 @@ client.on('clientReady', async () => {
             filePath = path.join(__dirname, 'ses.wav');
         }
 
-        console.log(`>>> Oynatılacak Dosya: ${filePath}`);
+        console.log(`>>> Oynatılacak Dosya Bulundu: ${filePath}`);
 
         const connection = joinVoiceChannel({
             channelId: channel.id,
             guildId: channel.guild.id,
             adapterCreator: channel.guild.voiceAdapterCreator,
             selfDeaf: false,
-            selfMute: false
+            selfMute: false,
+            group: client.user.id
         });
 
-        // UDP Bağlantısının TAM olarak kurulmasını bekliyoruz (Maksimum 15 sn)
-        try {
-            await entersState(connection, VoiceConnectionStatus.Ready, 15_000);
-            console.log(">>> SES KANALI UDP BAĞLANTISI TAMAMLANDI!");
-        } catch (e) {
-            console.log(">>> Uyarı: VoiceConnectionStatus.Ready beklemesi zaman aşımına uğradı, oynatma deneniyor...");
-        }
-
-        const player = createAudioPlayer();
+        const player = createAudioPlayer({
+            behaviors: {
+                noSubscriber: NoSubscriberBehavior.Play
+            }
+        });
 
         function play() {
+            // Volume modunu false yaparak işlemci karmakarışık ses işleme katmanını atlayıp doğrudan FFmpeg akışı basıyoruz
             const resource = createAudioResource(filePath, {
                 inputType: StreamType.Arbitrary,
                 ffmpegPath: ffmpegPath,
-                inlineVolume: true
+                inlineVolume: false 
             });
-
-            if (resource.volume) {
-                resource.volume.setVolume(1.0);
-            }
 
             player.play(resource);
         }
 
         connection.subscribe(player);
+
+        // Bağlantı koparsa veya kurulursa dinle
+        connection.on(VoiceConnectionStatus.Ready, () => {
+            console.log(">>> [BAĞLANTI BAŞARILI] Discord UDP Bağlantısı Kuruldu!");
+        });
+
+        // Oynatmayı doğrudan başlat
         play();
 
         player.on(AudioPlayerStatus.Playing, () => {
-            console.log(">>> [SONUÇ] SES AKIŞI AKTİF - KANALDA DUYULMALI!");
+            console.log(">>> [BAŞARILI] Sinyal gönderiliyor, ses çalıyor!");
         });
 
         player.on(AudioPlayerStatus.Idle, () => {
-            console.log(">>> Ses bitti, tekrar çalınıyor...");
+            console.log(">>> Ses bitti, başa sarılıyor...");
             play();
         });
 
